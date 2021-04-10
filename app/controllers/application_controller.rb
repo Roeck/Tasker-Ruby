322f35db1a8b64640ddf008a048aec983d2ac41f
Roeck <rons9n@gmail.com>
2021-04-10 18:29:34 -0400

    Signup functionality with tasks view routing done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,24 @@
-require './config/environment'
+require "./config/environment"
 
 class ApplicationController < Sinatra::Base
-
   configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
+    set :public_folder, "public"
+    set :views, "app/views"
+    enable :sessions
+    set :session_secret, "Task_secret"
+    register Sinatra::Flash
   end
 
   get "/" do
     erb :"/home.html"
   end
+  helpers do
+    def signed_in?
+      !!current_user
+    end
 
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,36 @@
 class UsersController < ApplicationController
+
+  # Signup routing:
+
   get "/signup" do
     erb :"/users/signup.html"
   end
+
+  # If Signup form submits empty inputs, alert requirement to user and refresh page:
+
+  post "/signup" do
+    if params[:name].empty? || params[:email].empty? || params[:password].empty?
+      redirect "/signup"
+
+  # Else, create a new instance of user using params and redirect to tasks page:
+    
+    else
+      @user = User.create(
+      :name => params[:name],
+      :email => params[:email],
+      :password => params[:password])
+      session[:user_id] = @user.id     
+      redirect "/tasks"
+    end
+  end
+
+  # If user is signed up, redirect to tasks page:
+
+  get "/tasks" do
+    @users = User.all
+    erb :'/tasks/tasks.html'
+  end
+
 end
+
+
