5f2f7c0ab99018237d81e1f1abbca6e194cd047c
Roeck <rons9n@gmail.com>
2021-04-12 17:56:42 -0400

    Task fetching done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,14 +9,41 @@ class TasksController < ApplicationController
     # New task route, if user is signed in:
 
     get "/tasks/new" do
-     if signed_in?
-        @user = User.find_by(id: session[:user_id])
-        erb :"/tasks/new.html"
+        if signed_in?
+            @user = User.find_by(id: session[:user_id])
+            erb :"/tasks/new.html"
 
-     # If not signed in, redirect user to sign in page:
-     
-     else
+        # If not signed in, redirect user to sign in page:
+        
+        else
+            redirect "/signin"
+        end
+    end
+
+    # Fetching and displaying tasks:
+
+    post "/tasks" do
+        # Raise params.inspect,
+        # Params {"chore"=>"raise params inspect"}:
+        if signed_in?
+        @user = User.find(session[:user_id])
+
+        if params[:chore].empty?
+            redirect "/tasks/new"
+        else
+            @user = User.find_by(:id => session[:user_id])
+            # Create new instance of task:
+            @task = Task.new
+            # Set the name of chore:
+            @task.chore = params[:chore]
+            # Save it:
+            @task.user_id = @user.id
+            @task.save
+            redirect "/tasks"
+        end
+        else
         redirect "/signin"
-     end
-  end
+        end
+    end
+
 end
