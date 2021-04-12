ef6e76c8367b15c63cc62a09cf9fec6fc15182c5
Roeck <rons9n@gmail.com>
2021-04-12 16:53:01 -0400

    New task route and initial view

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,22 @@
 class TasksController < ApplicationController
+    
+    # Tasks view route:
+
     get "/tasks" do
         erb :"/tasks/tasks"
     end
+
+    # New task route, if user is signed in:
+
+    get "/tasks/new" do
+     if signed_in?
+        @user = User.find_by(id: session[:user_id])
+        erb :"/tasks/new.html"
+
+     # If not signed in, redirect user to sign in page:
+     
+     else
+        redirect "/signin"
+     end
+  end
 end
